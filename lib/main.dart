e620e718a39e4bb850887004be9bcfe8c6d24edd
s14t284 <rikeda71@gmail.com>
2022-08-14 22:12:49 +0900

    :heavy_plus_sign: add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:virtualpilgrimage/gen/firebase_options_dev.dart' as dev;
@@ -7,6 +8,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
   await Firebase.initializeApp(options: getFirebaseOptions(flavor));
+  // FIXME: FirebaseAnalytics のテストのために記述しているので削除する
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  await analytics
+      .logEvent(name: 'start', parameters: {'foo': 'bar', 'hoge': 'fuga'});
   runApp(const MyApp());
 }
 
