fe300e7d7912c206b081a2e2d5adf9a1091daded
s14t284 <rikeda71@gmail.com>
2022-08-14 21:38:16 +0900

    :technologist: unify package name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:virtual_pilgrimage/gen/firebase_options_dev.dart' as dev;
-import 'package:virtual_pilgrimage/gen/firebase_options_prod.dart' as prod;
+import 'package:virtualpilgrimage/gen/firebase_options_dev.dart' as dev;
+import 'package:virtualpilgrimage/gen/firebase_options_prod.dart' as prod;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
