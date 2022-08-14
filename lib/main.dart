1bbdc25239ce863b54a689906a4aae6931f33354
s14t284 <rikeda71@gmail.com>
2022-08-14 21:24:53 +0900

    :sparkles: add firebase_core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:virtual_pilgrimage/gen/firebase_options_dev.dart' as dev;
+import 'package:virtual_pilgrimage/gen/firebase_options_prod.dart' as prod;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
+  await Firebase.initializeApp(options: getFirebaseOptions(flavor));
   runApp(const MyApp());
 }
 
@@ -111,5 +117,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+FirebaseOptions getFirebaseOptions(String flavor) {
+  switch (flavor) {
+    case 'dev':
+      return dev.DefaultFirebaseOptions.currentPlatform;
+    case 'prod':
+      return prod.DefaultFirebaseOptions.currentPlatform;
+    default:
+      throw ArgumentError(
+          'Flavor is invalid. "dev" or "prod" are expected. but flavor: $flavor');
   }
 }
