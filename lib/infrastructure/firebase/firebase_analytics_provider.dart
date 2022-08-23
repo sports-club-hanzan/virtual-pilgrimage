002469f56c438ce4a428e5d065f6ccea7bcf2a0b
s14t284 <rikeda71@gmail.com>
2022-08-23 23:52:23 +0900

    :construction: add firebase provider

---
+++ lib/infrastructure/firebase/firebase_analytics_provider.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final firebaseAnalyticsProvider = Provider<FirebaseAnalytics>(
+  (_) => FirebaseAnalytics.instance,
+);
