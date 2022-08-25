328d51e02c3795ed4bb007b2531f409741b98717
s14t284 <rikeda71@gmail.com>
2022-08-26 00:43:31 +0900

    :construction: updater logger

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,48 +1,9 @@
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:logger/logger.dart' as lib;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class Logger {
-  final lib.Logger _logger = lib.Logger(
-    printer: lib.PrettyPrinter(
-      methodCount: 1, // 表示されるコールスタックの数
-      errorMethodCount: 5, // 表示されるスタックトレースのコールスタックの数
-      lineLength: 120, // 出力するログ1行の幅
-      colors: true, // メッセージに色をつけるかどうか
-      printEmojis: true, // 絵文字を出力するかどうか
-      printTime: true, // タイムスタンプを出力するかどうか
-    ),
-  );
-
-  final FirebaseCrashlytics _crashlytics;
-
-  final String _className;
-
-  Logger(this._crashlytics, this._className);
-
-  debug(dynamic message) {
-    _logger.d(message);
-  }
-
-  info(dynamic message) {
-    _logger.i(message);
-    _crashlytics.log('[_className][$_className] $message');
-  }
-
-  warn(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.w(message, [error, stackTrace]);
-    _crashlytics.log('[_className][$_className] $message');
-    _crashlytics.recordError(error, stackTrace);
-  }
-
-  error(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.e(message, [error, stackTrace]);
-    _crashlytics.log('[_className][$_className] $message');
-    _crashlytics.recordError(error, stackTrace);
-  }
-}
-
-final logger = lib.Logger(
-  printer: lib.PrettyPrinter(
+final _logger = Logger(
+  printer: PrettyPrinter(
     methodCount: 1, // 表示されるコールスタックの数
     errorMethodCount: 5, // 表示されるスタックトレースのコールスタックの数
     lineLength: 120, // 出力するログ1行の幅
@@ -50,4 +11,12 @@ final logger = lib.Logger(
     printEmojis: true, // 絵文字を出力するかどうか
     printTime: true, // タイムスタンプを出力するかどうか
   ),
+);
+
+final loggerProider = Provider.autoDispose<Logger>(
+  (_) => _logger,
+);
+
+final crashlyticsProvider = Provider.autoDispose<FirebaseCrashlytics>(
+  (_) => FirebaseCrashlytics.instance,
 );
