dfeab61190cab62307382b2254b9be1fe3ca8019
s14t284 <rikeda71@gmail.com>
2022-08-27 19:42:50 +0900

    :sparkles: implement into sign_in page to router

--- lib/app.dart
+++ lib/app.dart
@@ -1,53 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:virtualpilgrimage/ui/pages/login/login_page.dart';
+import 'package:virtualpilgrimage/domain/auth/sign_in_controller.dart';
+import 'package:virtualpilgrimage/domain/auth/sign_in_state.codegen.dart';
+import 'package:virtualpilgrimage/ui/pages/login/signin_page.dart';
 import 'package:virtualpilgrimage/ui/style/theme.dart';
 
 class App extends ConsumerWidget {
   App({Key? key}) : super(key: key);
 
   // ref. https://zenn.dev/mkikuchi/articles/cc87c84e1404c4
-  final GoRouter _router = GoRouter(
-    routes: <GoRoute>[
-      // ルート
-      GoRoute(
-        path: '/',
-        // builder: (BuildContext context, GoRouterState state) {
-        //   return const Scaffold(
-        //     body: Text('home'),
-        //   );
-        // },
-        pageBuilder: (context, state) => MaterialPage<void>(
-          key: state.pageKey,
-          child: LoginPage(key: state.pageKey),
-        ),
-      ),
-      // ログイン
-      GoRoute(
-        path: '/login',
-        builder: (BuildContext context, GoRouterState state) {
-          return const LoginPage();
-        },
-      )
-    ],
-    redirect: ((state) {
-      // TODO: ログイン状態によって遷移先のページを変更する
-      if (state.location != '/login') {
-        return '/login';
-      }
-      return null;
-    }),
-    // FIXME: 以下にログイン状態を保つモデルを渡す。ref.watch で login状態を持つProviderを参照すれば良い
-    // refreshListenable:
-  );
+  final Provider<GoRouter> _router = Provider<GoRouter>((ref) => GoRouter(
+        routes: <GoRoute>[
+          // ルート
+          GoRoute(
+            path: '/',
+            // builder: (BuildContext context, GoRouterState state) {
+            //   return const Scaffold(
+            //     body: Text('home'),
+            //   );
+            // },
+            pageBuilder: (context, state) => MaterialPage<void>(
+              key: state.pageKey,
+              child: SigninPage(key: state.pageKey),
+            ),
+          ),
+          // ログイン
+          GoRoute(
+            path: '/signin',
+            builder: (BuildContext context, GoRouterState state) {
+              return const SigninPage();
+            },
+          )
+        ],
+        redirect: ((state) {
+          // サインイン状態によって遷移先を変える
+          final signInContext = ref.read(signInController).context;
+
+          if (signInContext == SignInStateContext.notSignedIn) {
+            return '/signin';
+          }
+          // ログイン成功時
+          if (signInContext == SignInStateContext.success) {}
+          // TODO: ログイン状態によって遷移先のページを変更する
+          if (state.location != '/signin') {
+            return '/signin';
+          }
+          return null;
+        }),
+      ));
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(_router);
+
     return MaterialApp.router(
-      routeInformationProvider: _router.routeInformationProvider,
-      routeInformationParser: _router.routeInformationParser,
-      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
       // TODO: タイトルはアプリ名に変更
       title: 'Virtual Pilgrimage',
       locale: const Locale('ja'),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:virtualpilgrimage/app.dart';
 import 'package:virtualpilgrimage/gen/firebase_options_dev.dart' as dev;
 import 'package:virtualpilgrimage/gen/firebase_options_prod.dart' as prod;
@@ -13,7 +14,7 @@ Future<void> main() async {
   final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   await analytics
       .logEvent(name: 'start', parameters: {'foo': 'bar', 'hoge': 'fuga'});
-  runApp(App());
+  runApp(ProviderScope(child: App()));
 }
 
 FirebaseOptions getFirebaseOptions(String flavor) {
