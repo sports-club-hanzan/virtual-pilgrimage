a42daa80d5caacb9340b850ad46b899f83499b25
s14t284 <rikeda71@gmail.com>
2022-08-28 16:26:14 +0900

    :sparkles: implement sign_in usecase

---
+++ lib/domain/auth/sign_in_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:virtualpilgrimage/domain/auth/auth_repository.dart';
+import 'package:virtualpilgrimage/domain/auth/sign_in_interactor.dart';
+import 'package:virtualpilgrimage/domain/user/user_repository.dart';
+import 'package:virtualpilgrimage/domain/user/virtual_pilgrimage_user.codegen.dart';
+import 'package:virtualpilgrimage/infrastructure/firebase/firebase_auth_provider.dart';
+import 'package:virtualpilgrimage/logger.dart';
+
+final signInUsecaseProvider = Provider.autoDispose<SignInUsecase>(
+  (ref) => SignInInteractor(
+    ref.watch(emailAndPasswordAuthRepositoryProvider),
+    ref.watch(googleAuthRepositoryProvider),
+    ref.watch(userRepositoryProvider),
+    ref.watch(loggerProider),
+    ref.watch(crashlyticsProvider),
+    ref.watch(firebaseAuthProvider),
+  ),
+);
+
+abstract class SignInUsecase {
+  Future<VirtualPilgrimageUser> signInWithGoogle();
+  Future<VirtualPilgrimageUser> signInWithEmailAndPassword(
+      String email, String password);
+  Future<void> logout();
+}
