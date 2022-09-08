e62763288da79e1dc9a771df857ff2f7905de034
itomizu <info@itomizu.dev>
2022-09-09 01:41:58 +0900

    ユーザ情報登録ページのレイアウト整理

--- lib/ui/pages/registration/registration_page.dart
+++ lib/ui/pages/registration/registration_page.dart
@@ -35,100 +35,110 @@ class RegistrationPageBody extends StatelessWidget {
     final notifier = ref.read(registrationPresenterProvider.notifier);
     final state = ref.watch(registrationPresenterProvider);
 
-    return ColoredBox(
-      color: Theme.of(context).backgroundColor,
-      child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
+    return ListView(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Text('ニックネーム'),
+              const Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+              ),
+              MyTextFormField(
+                formModel: state.nickname,
+                onChanged: notifier.onChangedNickname,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  labelStyle: TextStyle(
+                    color: state.nickname.focusNode.hasFocus
+                        ? Theme.of(context).primaryColor
+                        : ColorStyle.text,
+                    fontWeight: state.nickname.focusNode.hasFocus
+                        ? FontWeight.bold
+                        : FontWeight.normal,
+                  ),
+                  hintText: 'ニックネーム',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: const BorderSide(
+                      color: ColorStyle.white,
+                      width: 2,
+                    ),
+                  ),
+                  focusColor: Theme.of(context).primaryColor,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).primaryColor,
+                      width: 2,
+                    ),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.account_circle,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 10, 0, 10),
+              ),
+              const Text('性別'),
+              const Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+              ),
+              GenderRadioButtons(
+                radioButtonModel: state.gender,
+                onChanged: notifier.onChangedGender,
+                groupValue: state.gender.selectedValue,
+              ),
+              const Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
+              ),
+              const Text('生年月日'),
+              TextButton(
+                onPressed: () => notifier.onPressedDate(context),
+                child: Text(
+                  DateFormat('yyyy/MM/dd').format(state.birthDay),
+                  style: const TextStyle(color: Colors.black, fontSize: 25),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
                 top: 32,
                 right: 12,
                 left: 12,
                 bottom: 12,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('ニックネーム'),
-                  const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                  ),
-                  MyTextFormField(
-                    formModel: state.nickname,
-                    onChanged: notifier.onChangedNickname,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelStyle: TextStyle(
-                        color: state.nickname.focusNode.hasFocus
-                            ? Theme.of(context).primaryColor
-                            : ColorStyle.text,
-                        fontWeight: state.nickname.focusNode.hasFocus
-                            ? FontWeight.bold
-                            : FontWeight.normal,
-                      ),
-                      hintText: 'ニックネーム',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: const BorderSide(
-                          color: ColorStyle.white,
-                          width: 2,
-                        ),
-                      ),
-                      focusColor: Theme.of(context).primaryColor,
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        borderSide: BorderSide(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.account_circle,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(10, 10, 0, 10),
-                  ),
-                  const Text('性別'),
-                  const Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                  ),
-                  GenderRadioButtons(
-                    radioButtonModel: state.gender,
-                    onChanged: notifier.onChangedGender,
-                    groupValue: state.gender.selectedValue,
-                  ),
-                  const Text('生年月日'),
-                  TextButton(
-                    onPressed: () => notifier.onPressedDate(context),
-                    child: Text(
-                      DateFormat('yyyy/MM/dd').format(state.birthDay),
-                      style: const TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ],
+                child: Column(),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await notifier.onPressedRegistration();
-              },
-              child: const Text('ユーザ情報登録'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await ref.read(signInPresenterProvider.notifier).logout();
-                ref.read(routerProvider).go(RouterPath.signIn);
-              },
-              child: const Text('サインイン画面に戻る'),
-            )
-          ],
+              ElevatedButton(
+                onPressed: () async {
+                  await notifier.onPressedRegistration();
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: const Text('登録'),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  await ref.read(signInPresenterProvider.notifier).logout();
+                  ref.read(routerProvider).go(RouterPath.signIn);
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: const Text('ログイン画面'),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
