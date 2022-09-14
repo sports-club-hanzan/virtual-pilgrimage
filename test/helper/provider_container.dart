245caf33ec215926f1727e48012f75d5bd013455
s14t284 <rikeda71@gmail.com>
2022-09-14 22:57:21 +0900

    :adhesive_bandage: add firebase analytics mock for unittest

--- test/helper/provider_container.dart
+++ test/helper/provider_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:virtualpilgrimage/domain/auth/auth_repository.dart';
+import 'package:virtualpilgrimage/infrastructure/firebase/firebase_analytics_provider.dart';
 import 'package:virtualpilgrimage/infrastructure/firebase/firebase_auth_provider.dart';
 import 'package:virtualpilgrimage/infrastructure/firebase/firebase_crashlytics_provider.dart';
 import 'package:virtualpilgrimage/infrastructure/firebase/firestore_provider.dart';
@@ -9,9 +10,10 @@ import 'mock.mocks.dart';
 final defaultOverrides = <Override>[
   // firebase
   firebaseAuthProvider.overrideWithValue(MockFirebaseAuth()),
-  firebaseCrashlyticsProvider.overrideWithValue(MockFirebaseCrashlytics()),
   firestoreProvider.overrideWithValue(MockFirebaseFirestore()),
   firebaseAuthUserStateProvider.overrideWithValue(StateController(MockUser())),
+  firebaseCrashlyticsProvider.overrideWithValue(MockFirebaseCrashlytics()),
+  firebaseAnalyticsProvider.overrideWithValue(MockFirebaseAnalytics()),
   // google signIn
   googleSignInProvider.overrideWithValue(MockGoogleSignIn()),
 ];
