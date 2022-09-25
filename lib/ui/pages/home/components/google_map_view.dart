de6e30e4236ed77326c1c7cfcce5df5b6328d783
Ryuya Ikeda <rikeda71@gmail.com>
2022-09-25 23:17:30 +0900

    :recycle: split google map view and home page

---
+++ lib/ui/pages/home/components/google_map_view.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:virtualpilgrimage/ui/pages/home/home_presenter.dart';
+
+// TODO(s14t284): お試しで Google Map を表示しているだけであるため、必要に応じて修正する
+class GoogleMapView extends StatelessWidget {
+  const GoogleMapView(this._ref, {super.key});
+
+  final WidgetRef _ref;
+
+  // FIXME: 一時的に用意しているだけ
+  static const initialCameraPosition = CameraPosition(
+    target: LatLng(34.1597388, 134.4675072),
+    zoom: 10,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final state = _ref.watch(homeProvider).googleMap;
+    final notifier = _ref.read(homeProvider.notifier);
+
+    return SizedBox(
+      height: 350, // FIXME: 適当に固定値を設定しているので修正する
+      child: GoogleMap(
+        myLocationEnabled: false,
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        zoomGesturesEnabled: true,
+        tiltGesturesEnabled: false,
+        scrollGesturesEnabled: true,
+        mapType: MapType.normal,
+        initialCameraPosition: initialCameraPosition,
+        markers: state.markers,
+        polylines: state.polylines,
+        onMapCreated: notifier.onMapCreated,
+        // スクロールジェスチャーが正常にできるような設定
+        gestureRecognizers: const {
+          Factory<OneSequenceGestureRecognizer>(
+            EagerGestureRecognizer.new,
+          )
+        },
+      ),
+    );
+  }
+}
--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:virtualpilgrimage/domain/user/virtual_pilgrimage_user.codegen.dart';
 import 'package:virtualpilgrimage/router.dart';
-import 'package:virtualpilgrimage/ui/pages/home/home_presenter.dart';
+import 'package:virtualpilgrimage/ui/pages/home/components/google_map_view.dart';
 import 'package:virtualpilgrimage/ui/pages/sign_in/sign_in_presenter.dart';
 import 'package:virtualpilgrimage/ui/style/color.dart';
 
-// TODO(s14t284): 仮でユーザの情報を表示しているだけ
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
@@ -32,40 +30,10 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final userState = _ref.watch(userStateProvider);
-    final state = _ref.watch(homeProvider);
-    // FIXME: unused な変数じゃなくなったらコメントを削除
-    // MEMO: 現在はunusedな変数だが、ここで呼び出すことで初期化処理が走り、歩数の記録を行う
-    // ignore: unused_local_variable
-    final notifier = _ref.read(homeProvider.notifier);
-    // ignore: unused_local_variable
-    late GoogleMapController mapController;
-    const initialCameraPosition = CameraPosition(
-      target: LatLng(34.1597388, 134.4675072),
-      zoom: 10,
-    );
-    final markers = {
-      const Marker(
-        markerId: MarkerId('霊山寺'),
-        position: LatLng(34.15944444, 134.503),
-        infoWindow: InfoWindow(title: '霊峰寺', snippet: '1箇所目'),
-      ),
-      const Marker(
-        markerId: MarkerId('極楽寺'),
-        position: LatLng(34.15565, 134.490347),
-        infoWindow: InfoWindow(title: '極楽寺', snippet: '2箇所目'),
-      ),
-      Marker(
-        markerId: const MarkerId('ユーザ'),
-        position: const LatLng(34.10, 134.467),
-        icon: userState!.userIcon,
-        infoWindow: InfoWindow(title: '現在: ${userState.health?.totalSteps ?? 0}歩'),
-      )
-    };
 
     return ColoredBox(
       color: Theme.of(context).backgroundColor,
       child: SafeArea(
-        // TODO(s14t284): お試しで Google Map を表示しているだけであるため、必要に応じて修正する
         child: ListView(
           children: [
             const Text(
@@ -78,28 +46,28 @@ class HomePageBody extends StatelessWidget {
             Column(
               children: [
                 Text(
-                  'nickname: ${userState.nickname}',
+                  'nickname: ${userState?.nickname ?? ''}',
                   style: const TextStyle(
                     fontSize: 24,
                     color: ColorStyle.grey,
                   ),
                 ),
                 Text(
-                  'email: ${userState.email}',
+                  'email: ${userState?.email ?? ''}',
                   style: const TextStyle(
                     fontSize: 24,
                     color: ColorStyle.grey,
                   ),
                 ),
                 Text(
-                  'gender: ${userState.gender}',
+                  'gender: ${userState?.gender ?? ''}',
                   style: const TextStyle(
                     fontSize: 24,
                     color: ColorStyle.grey,
                   ),
                 ),
                 Text(
-                  'birthday: ${DateFormat('yyyy/MM/dd').format(userState.birthDay)}',
+                  'birthday: ${userState != null ? DateFormat('yyyy/MM/dd').format(userState.birthDay) : ''}',
                   style: const TextStyle(
                     fontSize: 24,
                     color: ColorStyle.grey,
@@ -117,20 +85,7 @@ class HomePageBody extends StatelessWidget {
             const Padding(
               padding: EdgeInsetsDirectional.all(16),
             ),
-            SizedBox(
-              height: 350,
-              child: GoogleMap(
-                myLocationEnabled: false,
-                myLocationButtonEnabled: false,
-                mapType: MapType.normal,
-                initialCameraPosition: initialCameraPosition,
-                markers: markers,
-                polylines: state.polylines,
-                onMapCreated: (GoogleMapController controller) {
-                  mapController = controller;
-                },
-              ),
-            ),
+            GoogleMapView(_ref),
           ],
         ),
       ),
