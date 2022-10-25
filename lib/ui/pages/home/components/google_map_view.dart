64abeaa24f289460a5e1bc4c0a986bbe5388c290
Ryuya Ikeda <rikeda71@gmail.com>
2022-10-25 19:53:20 +0900

    :recycle: refactor google map view

--- lib/ui/pages/home/components/google_map_view.dart
+++ lib/ui/pages/home/components/google_map_view.dart
@@ -6,10 +6,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:virtualpilgrimage/ui/pages/home/home_presenter.dart';
 
 // TODO(s14t284): お試しで Google Map を表示しているだけであるため、必要に応じて修正する
-class GoogleMapView extends StatelessWidget {
-  const GoogleMapView(this._ref, {super.key});
-
-  final WidgetRef _ref;
+class GoogleMapView extends ConsumerWidget {
+  const GoogleMapView({super.key});
 
   // FIXME: 一時的に用意しているだけ
   static const initialCameraPosition = CameraPosition(
@@ -18,9 +16,9 @@ class GoogleMapView extends StatelessWidget {
   );
 
   @override
-  Widget build(BuildContext context) {
-    final state = _ref.watch(homeProvider);
-    final notifier = _ref.read(homeProvider.notifier);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(homeProvider);
+    final notifier = ref.read(homeProvider.notifier);
 
     return SizedBox(
       height: 350, // FIXME: 適当に固定値を設定しているので修正する
