15dcb2d295b9b61c9e0a140d37e95accaafad875
Ryuya Ikeda <rikeda71@gmail.com>
2022-11-03 22:22:45 +0900

    :sparkles: load environment variables and split flavor in ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // GoogleMap用の設定
+    let googleApiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAP_API_KEY") as! String
+    GMSServices.provideAPIKey(googleApiKey)
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
