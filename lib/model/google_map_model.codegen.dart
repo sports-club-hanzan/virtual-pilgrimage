ea9021ac813e927fff64a8e11dfdf2317e0cf9a8
Ryuya Ikeda <rikeda71@gmail.com>
2022-11-06 15:32:25 +0900

    :fire: remove unused method

--- lib/model/google_map_model.codegen.dart
+++ lib/model/google_map_model.codegen.dart
@@ -17,22 +17,4 @@ class GoogleMapModel with _$GoogleMapModel {
   const GoogleMapModel._();
 
   void onMapCreated(GoogleMapController controller) => this.controller.complete(controller);
-
-  // ignore: prefer_constructors_over_static_methods
-  static GoogleMapModel initialize() => GoogleMapModel(
-        controller: Completer(),
-        // FIXME: 適当に埋めているだけであるため、firestore or cache からお遍路の情報を引っ張ってくるようにする
-        markers: {
-          const Marker(
-            markerId: MarkerId('霊山寺'),
-            position: LatLng(34.15944444, 134.503),
-            infoWindow: InfoWindow(title: '霊峰寺', snippet: '1箇所目'),
-          ),
-          const Marker(
-            markerId: MarkerId('極楽寺'),
-            position: LatLng(34.15565, 134.490347),
-            infoWindow: InfoWindow(title: '極楽寺', snippet: '2箇所目'),
-          ),
-        },
-      );
 }
