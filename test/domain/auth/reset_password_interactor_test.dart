b59e9dfecee917791224ef5ca8b985623bb2e5b1
Ryuya Ikeda <rikeda71@gmail.com>
2022-11-06 18:55:02 +0900

    :sparkles: implement reset password usecase

---
+++ test/domain/auth/reset_password_interactor_test.dart
@@ -0,0 +1,88 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:logger/logger.dart';
+import 'package:mockito/mockito.dart';
+import 'package:virtualpilgrimage/domain/auth/reset_password_interactor.dart';
+import 'package:virtualpilgrimage/domain/auth/reset_password_usecase.dart';
+
+import '../../helper/fakes/fake_package_info.dart';
+import '../../helper/mock.mocks.dart';
+import '../../helper/provider_container.dart';
+
+void main() {
+  late ResetUserPasswordInteractor target;
+  late MockEmailAndPasswordAuthRepository emailAndPasswordAuthRepository;
+  late MockFirebaseCrashlytics crashlytics;
+  final logger = Logger(level: Level.nothing);
+
+  const email = 'test@example.com';
+  const packageName = 'com.virtualpilgrimage';
+
+  setUp(() {
+    emailAndPasswordAuthRepository = MockEmailAndPasswordAuthRepository();
+    crashlytics = MockFirebaseCrashlytics();
+    target = ResetUserPasswordInteractor(emailAndPasswordAuthRepository, logger, crashlytics);
+
+    initializeFakePackageInfo(packageName: packageName);
+  });
+
+  tearDown(() {
+    const MethodChannel('plugins.flutter.io/package_info').setMockMethodCallHandler(null);
+  });
+
+  group('ResetPasswordInteractor', () {
+    test('DI', () {
+      final container = mockedProviderContainer();
+      final usecase = container.read(resetUserPasswordUsecaseProvider);
+      expect(usecase, isNotNull);
+    });
+
+    group('execute', () {
+      test('正常系', () async {
+        // given
+        when(emailAndPasswordAuthRepository.resetPassword(email: email, packageName: packageName))
+            .thenAnswer((_) => Future.value());
+
+        // then
+        await target.execute(email: email);
+
+        // then
+        verify(emailAndPasswordAuthRepository.resetPassword(email: email, packageName: packageName))
+            .called(1);
+        verifyNever(crashlytics.recordError(any, any)).called(0);
+      });
+
+      group('異常系', () {
+        final params = {
+          'FirebaseException': FirebaseException(plugin: ''),
+          'Exception': Exception(),
+        };
+
+        for (final param in params.entries) {
+          test(param.key, () async {
+            // given
+            when(
+              emailAndPasswordAuthRepository.resetPassword(
+                email: anyNamed('email'),
+                packageName: anyNamed('packageName'),
+              ),
+            ).thenThrow(param.value);
+
+            // when
+            await target.execute(email: email);
+
+            // then
+            verify(
+              emailAndPasswordAuthRepository.resetPassword(
+                email: anyNamed('email'),
+                packageName: anyNamed('packageName'),
+              ),
+            ).called(1);
+            verify(crashlytics.recordError(param.value, any)).called(1);
+          });
+        }
+      });
+    });
+  });
+}
---
+++ test/helper/fakes/fake_package_info.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void initializeFakePackageInfo({
+  String appName = '巡礼ウォーク',
+  String packageName = 'com.virtualpilgrimage',
+  String version = '1.0.0',
+  String buildNumber = '1',
+}) {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  const MethodChannel('plugins.flutter.io/package_info')
+      .setMockMethodCallHandler((MethodCall methodCall) async {
+    if (methodCall.method == 'getAll') {
+      return <String, dynamic>{
+        'appName': appName,
+        'packageName': packageName,
+        'version': version,
+        'buildNumber': buildNumber,
+      };
+    }
+    return null;
+  });
+}
