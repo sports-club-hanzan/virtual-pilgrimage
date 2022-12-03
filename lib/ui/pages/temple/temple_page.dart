2e06729595de32b62cb51f64d945e53f95ce7a23
Ryuya Ikeda <rikeda71@gmail.com>
2022-12-03 17:07:05 +0900

    :sparkles: use temple presenter in temple page and add test of temple page

--- lib/ui/pages/temple/temple_page.dart
+++ lib/ui/pages/temple/temple_page.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:virtualpilgrimage/domain/pilgrimage/temple_info.codegen.dart';
 import 'package:virtualpilgrimage/domain/user/virtual_pilgrimage_user.codegen.dart';
-import 'package:virtualpilgrimage/infrastructure/pilgrimage/temple_repository_impl.dart';
 import 'package:virtualpilgrimage/ui/components/bottom_navigation.dart';
 import 'package:virtualpilgrimage/ui/components/molecules/my_drawer.dart';
 import 'package:virtualpilgrimage/ui/components/my_app_bar.dart';
 import 'package:virtualpilgrimage/ui/pages/temple/temple_detail_dialog.dart';
+import 'package:virtualpilgrimage/ui/pages/temple/temple_presenter.dart';
 import 'package:virtualpilgrimage/ui/style/font.dart';
 
 class TemplePage extends ConsumerWidget {
@@ -37,39 +37,40 @@ class _TemplePageBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final temples = ref.read(templeInfoCache);
+    final state = ref.watch(templeProvider).temples;
+    final scrollController = ref.read(templeProvider).scrollController;
     final user = ref.watch(userStateProvider);
 
     return ListView.builder(
+      controller: scrollController,
       itemBuilder: (BuildContext context, int index) {
-        return _buildTemple(context, temples[index + 1]!, user!);
+        return _buildTemple(context, state[index], user);
       },
-      itemCount: temples.length,
+      itemCount: state.length,
     );
   }
 
-  Widget _buildTemple(BuildContext context, TempleInfo templeInfo, VirtualPilgrimageUser user) {
-    final bool isShowDetail =
-        user.pilgrimage.nowPilgrimageId >= templeInfo.id || 1 < user.pilgrimage.lap;
+  Widget _buildTemple(BuildContext context, TempleInfo templeInfo, VirtualPilgrimageUser? user) {
+    // ユーザ情報を取得できなかった時のために、念の為デフォルト値を詰めておく
+    final nowPilgrimageId = user?.pilgrimage.nowPilgrimageId ?? 1;
+    final lap = user?.pilgrimage.lap ?? 1;
+    final isShowDetail = nowPilgrimageId >= templeInfo.id || 1 < lap;
+    // 画像が表示できない時にエラーとならないよう適当な画像を表示できるようにしておく
+    final imagePath = templeInfo.images.isNotEmpty
+        ? templeInfo.images[0]
+        : 'https://firebasestorage.googleapis.com/v0/b/virtual-pilgrimage-dev.appspot.com/o/temples%2F1%2F1.jpeg?alt=media&token=b3fe42f9-b94b-43f2-8a5d-b2f217be541f';
 
     return Card(
+      key: Key('temple_${templeInfo.id}'),
       elevation: 6,
       margin: const EdgeInsets.all(10),
       child: ListTile(
         leading: isShowDetail
-            ? Image(
-                width: 100,
-                height: 80,
-                image: NetworkImage(
-                  templeInfo.images[0],
-                ),
-              )
+            ? Image(width: 100, height: 80, image: NetworkImage(imagePath))
             : Image(
                 width: 100,
                 height: 80,
-                image: NetworkImage(
-                  templeInfo.images[0],
-                ),
+                image: NetworkImage(imagePath),
                 color: Colors.black45,
                 colorBlendMode: BlendMode.xor,
               ),
@@ -79,10 +80,10 @@ class _TemplePageBody extends StatelessWidget {
             Text(
               '${templeInfo.id}番札所',
               style: const TextStyle(
-                color: Colors.black87,
+                color: Colors.black38,
                 fontSize: FontSize.smallSize,
                 fontFamily: 'Poppins',
-                fontWeight: FontWeight.w200,
+                fontWeight: FontWeight.w500,
               ),
             ),
             Text(
@@ -98,10 +99,7 @@ class _TemplePageBody extends StatelessWidget {
         ),
         subtitle: Text(
           '${templeInfo.prefecture}・${templeInfo.distance}m',
-          style: const TextStyle(
-            color: Colors.black38,
-            fontSize: FontSize.smallSize,
-          ),
+          style: const TextStyle(color: Colors.black38, fontSize: FontSize.smallSize),
         ),
         onTap: () => {
           if (isShowDetail)
---
+++ test/ui/pages/temple/temple_page_test.dart
@@ -0,0 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+import 'package:virtualpilgrimage/application/pilgrimage/temple_repository.dart';
+import 'package:virtualpilgrimage/domain/pilgrimage/temple_info.codegen.dart';
+import 'package:virtualpilgrimage/infrastructure/pilgrimage/temple_repository_impl.dart';
+import 'package:virtualpilgrimage/ui/pages/temple/temple_page.dart';
+
+import '../../../application/pilgrimage/update_pilgrimage_progress_interactor_test.mocks.dart';
+import '../../../helper/provider_container.dart';
+import '../../../helper/wrap_material_app.dart';
+
+void main() {
+  final templeRepository = MockTempleRepository();
+  when(templeRepository.getTempleInfoAll()).thenAnswer((_) => Future.value());
+
+  // ダイアログの表示テストはwidgetテストの範囲に収まらないので、実装していない
+  group('TemplePage', () {
+    testWidgets('表示できる', (widgetTester) async {
+      // MEMO: mockNetworkImagesFor: Network通信をmock化してテスト
+      await mockNetworkImagesFor(
+        () => widgetTester.pumpWidget(
+          wrapMaterialApp(
+            mockedProviderScope(
+              const TemplePage(),
+              overrides: [
+                templeRepositoryProvider.overrideWithValue(templeRepository),
+                templeInfoCache.overrideWith((_) => createTempleInfoMap()),
+              ],
+            ),
+          ),
+        ),
+      );
+      expect(find.text('霊山寺'), findsOneWidget);
+      // スクロールしないと見えないはず
+      expect(find.text('大窪寺'), findsNothing);
+      // スクロールしないと6件までしか見えないはず
+      expect(find.text('徳島県・1400m'), findsNWidgets(6));
+    });
+
+    testWidgets('スクロールすると次の札所情報が表示される', (widgetTester) async {
+      await mockNetworkImagesFor(
+            () => widgetTester.pumpWidget(
+          wrapMaterialApp(
+            mockedProviderScope(
+              const TemplePage(),
+              overrides: [
+                templeRepositoryProvider.overrideWithValue(templeRepository),
+                templeInfoCache.overrideWith((_) => createTempleInfoMap()),
+              ],
+            ),
+          ),
+        ),
+      );
+
+      // ref. https://docs.flutter.dev/cookbook/testing/widget/scrolling
+      final listFinder = find.byType(Scrollable);
+      final itemFinder = find.byKey(const ValueKey('temple_88'));
+
+      await widgetTester.scrollUntilVisible(itemFinder, 500, scrollable: listFinder);
+
+      // 末尾のお寺情報が見えているはず
+      expect(itemFinder, findsOneWidget);
+      // 期待する情報が見えているか検証
+      expect(find.text('長尾寺'), findsOneWidget);
+      expect(find.text('高知県・1400m'), findsOneWidget);
+      expect(find.text('愛媛県・1400m'), findsOneWidget);
+      expect(find.text('香川県・1400m'), findsNWidgets(2));
+    });
+  });
+}
+
+Map<int, TempleInfo> createTempleInfoMap() => {
+      1: createTempleInfo(id: 1, name: '霊山寺'),
+      2: createTempleInfo(id: 2, name: '極楽寺'),
+      3: createTempleInfo(id: 3, name: '金泉寺'),
+      4: createTempleInfo(id: 4, name: '大日寺'),
+      5: createTempleInfo(id: 5, name: '地蔵寺'),
+      6: createTempleInfo(id: 6, name: '安楽寺'),
+      7: createTempleInfo(id: 7, name: '十楽寺'),
+      28: createTempleInfo(id: 28, name: '大日寺', prefecture: '高知県'),
+      41: createTempleInfo(id: 41, name: '龍光寺', prefecture: '愛媛県'),
+      87: createTempleInfo(id: 87, name: '長尾寺', prefecture: '香川県'),
+      88: createTempleInfo(id: 88, name: '大窪寺', prefecture: '香川県'),
+    };
+
+TempleInfo createTempleInfo({required int id, required String name, String prefecture = '徳島県'}) {
+  return TempleInfo(
+    id: id,
+    name: name,
+    prefecture: prefecture,
+    address: '$prefecture**市++町',
+    distance: 1400,
+    encodedPoints: '',
+    geoPoint: const GeoPoint(34, 134),
+  );
+}
